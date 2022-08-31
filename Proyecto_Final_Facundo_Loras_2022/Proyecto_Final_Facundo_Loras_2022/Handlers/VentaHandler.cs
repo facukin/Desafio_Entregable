fe3ce49ff7c593bc8521f2d72acbad95d68101cc
facukin <facu-loras@hotmail.com>
2022-08-31 16:39:12 -0300

    Correccion
    Al INSERT de Ventas, le agregué un INSERT a la tabla ProdudcotVendido y un UPDATE para el stock de Producto.

--- Proyecto_Final_Facundo_Loras_2022/Proyecto_Final_Facundo_Loras_2022/Handlers/VentaHandler.cs
+++ Proyecto_Final_Facundo_Loras_2022/Proyecto_Final_Facundo_Loras_2022/Handlers/VentaHandler.cs
@@ -74,7 +74,44 @@ namespace Clase_08_08.Handlers
                         sqlCommand.Parameters.Add(parametroIdVenta);
 
                         sqlCommand.ExecuteNonQuery();
+
+                        string queryInsert2 = "INSERT INTO [SistemaGestion].[dbo].[ProductoVendido] (Stock, IdProducto, IdVenta) VALUES (@stockNuevo, @idProductoNuevo, @idVentaNuevo);";
+                        ProductoVendido productoVendido = new ProductoVendido();
+
+                        SqlParameter parametroStockNuevo = new SqlParameter("stockNuevo", System.Data.SqlDbType.BigInt) { Value = productoVendido.Stock };
+                        SqlParameter parametroIdProductoNuevo = new SqlParameter("idProductoNuevo", System.Data.SqlDbType.BigInt) { Value = productoVendido.IdProducto };
+                        SqlParameter parametroIdVentaNuevo = new SqlParameter("idVentaNuevo", System.Data.SqlDbType.BigInt) { Value = productoVendido.IdVenta };
+
+                        using (SqlCommand sqlCommand2 = new SqlCommand(queryInsert2, sqlConnection))
+                        {
+                            sqlCommand2.Parameters.Add(parametroStockNuevo);
+                            sqlCommand2.Parameters.Add(parametroIdProductoNuevo);
+                            sqlCommand2.Parameters.Add(parametroIdVentaNuevo);
+
+                            sqlCommand2.ExecuteNonQuery();
+
+                            string queryUpdateP = "UPDATE [SistemaGestion].[dbo].[Producto] SET Stock = @stockNuevo WHERE Id = @idProd;";
+                            Producto producto = new Producto();
+
+                            SqlParameter parametroStockNuevoP = new SqlParameter();
+                            parametroStockNuevoP.ParameterName = "stockNuevo";
+                            parametroStockNuevoP.Value = producto.Stock;
+                            parametroStockNuevoP.SqlDbType = System.Data.SqlDbType.BigInt;
+
+                            SqlParameter parametroIdVentaP = new SqlParameter();
+                            parametroIdVentaP.ParameterName = "idProd";
+                            parametroIdVentaP.Value = producto.Id;
+                            parametroIdVentaP.SqlDbType = System.Data.SqlDbType.BigInt;
+
+                            using (SqlCommand sqlcommandP = new SqlCommand(queryUpdateP, sqlConnection))
+                            {
+                                sqlcommandP.Parameters.Add(parametroStockNuevoP);
+                                sqlcommandP.ExecuteScalar();
+                            }
+                        }
                     }
+
+                    sqlConnection.Close();
                 }
 
             }
